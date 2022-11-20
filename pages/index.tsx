f3da3926cabaed71353968665a140ec3dcbfda43
joonseokhu <vraimentres@gmail.com>
2022-11-20 13:03:20 +0900

    Fix: 타입 설정과 라우팅 이슈

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import format from 'date-fns/format';
-import { PostsType } from '~/types';
 import { getPosts } from '~/utils/getPosts';
 import styles from './main.module.css';
+import { PostListItem } from '~/types';
 
 const {
   mainContainer, title, titleLine, mainListGroup,
 } = styles;
 
-const Main = ({ posts }:any) => (
+const Main = ({ posts }: { posts: PostListItem[] }) => (
   <div className={mainContainer}>
     {/* // 날짜 형식은 어떻게 할지 다음에 고민해보지 뭐~~ */}
     <h1 className={title}>{format(new Date(), 'MMMM y')}</h1>
@@ -24,14 +24,14 @@ const Main = ({ posts }:any) => (
     <br />
     <ul className={mainListGroup}>
       {posts.map((post) => {
-        const { data, slug } = post;
-        const url = slug.join('/');
+        const { meta, slug } = post;
+        const url = `posts/${slug.join('/')}`;
         return (
           <React.Fragment key={url}>
             <a href={url}>
               <li className="underline decoration-wavy decoration-indigo-600">
-                <span>{data.title}</span>
-                <span>{data.date}</span>
+                <span>{meta.title}</span>
+                <span>{meta.date}</span>
               </li>
             </a>
           </React.Fragment>
@@ -45,17 +45,8 @@ export default Main;
 
 export const getStaticProps = async () => {
   const posts = await getPosts();
-  const paths = await posts.map((post) => (
-    {
-      params: {
-        slug: [post.slug],
-      },
-    }
-  ));
-  // console.log(JSON.stringify(paths, null, 2));
+
   return {
-    props: {
-      posts,
-    },
+    props: { posts },
   };
 };
